a2f6af94fe59e6961ebc7aa67b76634d5c3b5327
isaarinen <Petteri.Saarinen2@metropolia.fi>
2024-12-05 12:58:39 +0200

    flask session thing

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import SQL_functions
 import Game_functions
 import colors
 import connector
+import json
+from flask import Flask, Response, session
+from flask_cors import CORS
+
 # class
 class GameState:
     def __init__(self):
@@ -14,55 +18,95 @@ class GameState:
         self.position = 0
         self.session_id = 0
         self.score = 0
-status = GameState()
 # get connection time
 connector.print_connection_time()
-# set up username
-status.username = input('Enter your username: ')
-Game_functions.check_username(status)
-#set up session specific board
-SQL_functions.set_board_airports(status.session_id)
-SQL_functions.set_player_property(status.session_id)
+
+# GAME SETUP USING FLASK SESSIONS, START CREATES THE SESSION, PLAY SHOWS HOW TO RE-INITIALIZE SESSION WHEN DOING ANY FUNCTION
+app = Flask(__name__)
+CORS(app)
+@app.route('/gameapi/start/<username>')
+def start(username):
+    status = GameState()
+    status.username = username
+    Game_functions.check_username(status)
+
+    #set up session specific board
+    SQL_functions.set_board_airports(status.session_id)
+    SQL_functions.set_player_property(status.session_id)
+
+    session['game_state'] = {
+        "rounds": status.rounds,
+        "doubles": status.doubles,
+        "jail_card": status.jail_card,
+        "jail_counter": status.jail_counter,
+        "jailed": status.jailed,
+        "username": status.username,
+        "position": status.position,
+        "session_id": status.session_id,
+        "score": status.score
+    }
+    return status.session_id
+@app.route('/gameapi/play')
+def play():
+    # game state session, dont touch
+    game_state = session.get('game_state')
+    status = GameState()
+    for thing, value in game_state.items():
+        setattr(status, thing, value)
+    # code here:
+
+@app.errorhandler(404)
+def page_not_found(error_code):
+    response = {
+        "message": "Invalid endpoint",
+        "status": 404
+    }
+    json_response = json.dumps(response)
+    http_response = Response(response=json_response, status=404, mimetype="application/json")
+    return http_response
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
+
+#old main vvvvvvvv
+
 # position move and rounds up
-while status.rounds <= 20:
-    money = SQL_functions.get_money(status.session_id)
-    if money <= 0:
-        Game_functions.bankrupt(status.session_id)
-        break
-    else:
-        if status.jailed:
-            Game_functions.jail_event(status)
-        else:
-            Game_functions.print_player_property(status)
-            userinput = input(f'{colors.col.BOLD}Roll the dice 🎲 to move. Press any key to roll. {colors.col.END}')
-            Game_functions.developer_privileges(userinput, status)
-            dice_roll_1,dice_roll_2,status = Game_functions.roll_and_move(status)
-            Game_functions.dice_roll_result(dice_roll_1, dice_roll_2, status)
-            if Game_functions.check_if_double(dice_roll_1, dice_roll_2, status):
-                status.doubles += 1
-            else:
-                if status.doubles >= 2:
-                    Game_functions.roll_double(status)
-                    Game_functions.jail_event(status)
-                else:
-                    status.doubles = 0
-                    temp_type_id = SQL_functions.get_type_id(status.position)
-                    # Non-functional cells
-                    if temp_type_id == 0:
-                        Game_functions.non_functional_cell(status)
-                    # airport cell
-                    elif temp_type_id == 1:
-                        Game_functions.airport_cell(status)
-                    # Other cells
-                    elif temp_type_id == 2:
-                        Game_functions.chance_card(status)
-                    elif temp_type_id == 3:
-                        Game_functions.go_to_jail(status)
-                    elif temp_type_id == 4:
-                        Game_functions.income_tax(status.session_id)
-                    elif temp_type_id == 5:
-                        Game_functions.luxury_tax(status.session_id)
+# while status.rounds <= 20:
+#     money = SQL_functions.get_money(status.session_id)
+#     if money <= 0:
+#         Game_functions.bankrupt(status.session_id)
+#         break
+#     else:
+#         if status.jailed:
+#             Game_functions.jail_event(status)
+#         else:
+#             Game_functions.print_player_property(status)
+#             userinput = input(f'{colors.col.BOLD}Roll the dice 🎲 to move. Press any key to roll. {colors.col.END}')
+#             Game_functions.developer_privileges(userinput, status)
+#             dice_roll_1,dice_roll_2,status = Game_functions.roll_and_move(status)
+#             Game_functions.dice_roll_result(dice_roll_1, dice_roll_2, status)
+#             if Game_functions.check_if_double(dice_roll_1, dice_roll_2, status):
+#                 status.doubles += 1
+#             else:
+#                 if status.doubles >= 2:
+#                     Game_functions.roll_double(status)
+#                     Game_functions.jail_event(status)
+#                 else:
+#                     status.doubles = 0
+#                     temp_type_id = SQL_functions.get_type_id(status.position)
+#                     # Non-functional cells
+#                     if temp_type_id == 0:
+#                         Game_functions.non_functional_cell(status)
+#                     # airport cell
+#                     elif temp_type_id == 1:
+#                         Game_functions.airport_cell(status)
+#                     # Other cells
+#                     elif temp_type_id == 2:
+#                         Game_functions.chance_card(status)
+#                     elif temp_type_id == 3:
+#                         Game_functions.go_to_jail(status)
+#                     elif temp_type_id == 4:
+#                         Game_functions.income_tax(status.session_id)
+#                     elif temp_type_id == 5:
+#                         Game_functions.luxury_tax(status.session_id)
 #wininig of game
-if  status.rounds > 20:
-    Game_functions.print_won_game(status)
-    Game_functions.print_high_score(status)
